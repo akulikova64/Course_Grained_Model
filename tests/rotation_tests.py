ec69413aff938a82778b5df6ef458551cccf2ea6
Anastasiya Kulikova <akulikova82@yahoo.com>
2020-05-21 00:36:32 -0500

    updated .gitignore file

--- tests/rotation_tests.py
+++ tests/rotation_tests.py
@@ -1,1 +1,12 @@
+import sys
+import os
+sys.path.append(os.path.abspath(".."))
+
+import rotations
 # testing the rotation functions in rotations.py 
+
+def test():
+  assert (1, 2, 3) == (1, 2, 3)
+
+def test_2():
+  assert (1, 2, 3) == (3, 2, 1)

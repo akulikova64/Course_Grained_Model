233c9e4a2acd783fa40e934a14a121408fed7909
Anastasiya Kulikova <akulikova82@yahoo.com>
2020-06-11 09:09:16 -0500

    fixed imports in new modules

---
+++ src/plot_maker.py
@@ -0,0 +1,23 @@
+from matplotlib import pyplot as plt
+# plot-maker
+
+#graphing the accuracy and loss for both the training and test data
+def get_plots(history):
+  #summarize history for accuracy 
+  plt.plot(history.history['accuracy'])
+  plt.plot(history.history['val_accuracy'])
+  plt.title('model accuracy')
+  plt.ylabel('accuracy')
+  plt.xlabel('epoch')
+  plt.legend(['training', 'validation'], loc = 'upper left')
+  plt.savefig("Accuracy_cgm_flips.pdf")
+  plt.clf()
+
+  # summarize history for loss
+  plt.plot(history.history['loss'])
+  plt.plot(history.history['val_loss'])
+  plt.title('model loss')
+  plt.ylabel('loss')
+  plt.xlabel('epoch')
+  plt.legend(['training', 'validaton'], loc = 'upper left')
+  plt.savefig("Loss_cgm_flips.pdf")

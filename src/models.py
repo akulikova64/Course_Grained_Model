63e6edca1fcc99e3a25686f04e1049e5be8d71a3
Anastasiya Kulikova <akulikova82@yahoo.com>
2020-07-03 12:59:31 -0500

    updated model structures

--- src/models.py
+++ src/models.py
@@ -68,3 +68,20 @@ def model_3(GPUS = 1):
   model.compile(loss ='categorical_crossentropy', optimizer = Adam(lr = .001), metrics = ['accuracy'])
 
   return model
+
+  def model_4(GPUS = 1):
+  """ model with three medium conv layers, one dense layer and more neurons """
+  model = Sequential()
+  model.add(Convolution3D(30, kernel_size = (1, 1, 1), strides = (1, 1, 1), activation = 'relu', input_shape = (9, 9, 9, 20))) # 32 output nodes, kernel_size is your moving window, activation function, input shape = auto calculated
+  model.add(Convolution3D(60, (3, 3, 3), activation = 'relu')) 
+  model.add(Convolution3D(60, (3, 3, 3), activation = 'relu'))
+  model.add(Convolution3D(60, (3, 3, 3), activation = 'relu'))
+  model.add(Flatten()) # now our layers have been combined to one
+  model.add(Dense(1000, activation = 'relu')) # 500 nodes in the last hidden layer
+  model.add(Dense(20, activation = 'softmax')) # output layer has 20 possible classes (amino acids 0 - 19)
+
+  model = multi_gpu_model(model, gpus=GPUS)
+
+  model.compile(loss ='categorical_crossentropy', optimizer = Adam(lr = .001), metrics = ['accuracy'])
+
+  return model
